236c21068fe49e579a4cae8b81916a2e83a5fbfc
ali zarkesh <alimexico021@gmail.com>
2025-02-05 22:49:06 +0330

    add: router and err handler

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const adminrouts = require("./router/admin");
+const indexrouter = require("./router/index");
+const errorcontrol = require("./controller/error");
+const { setstatics } = require("./utils/static");
 const app = express();
 
 // middelewars
@@ -8,5 +12,15 @@ app.use(bodyparser.urlencoded({ extended: false }));
 //ejs
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+//static
+setstatics(app);
+
+//routers
+app.use("/admin", adminrouts);
+app.use(indexrouter);
+
+// err
+app.use(errorcontrol.get404);
 
 app.listen(3000, () => console.log("app is runnig..."));
